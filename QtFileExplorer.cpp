a262e2ea6264e94c454f32613431eff92dc2e5c6
權少 <ljqican@163.com>
2020-02-17 23:16:08 +0800

    左邊加入目錄樹功能

--- QtFileExplorer.cpp
+++ QtFileExplorer.cpp
@@ -37,6 +37,8 @@ QtFileExplorer::QtFileExplorer(QWidget *parent)
 
     ui.rightTreeWidget->setColumnHidden(Item::PATH, true);
 
+    ui.leftTreeWidget->setColumnHidden(1, true);
+
     auto header = ui.rightTreeWidget->header();
     header->setSectionResizeMode(Item::NAME, QHeaderView::Stretch);
     header->setSectionResizeMode(Item::SIZE, QHeaderView::Fixed);
@@ -45,6 +47,9 @@ QtFileExplorer::QtFileExplorer(QWidget *parent)
     enter("D:/");
 
     connect(ui.rightTreeWidget, &QTreeWidget::itemDoubleClicked,
+        this, &QtFileExplorer::on_itemDoubleClicked);
+
+    connect(ui.leftTreeWidget, &QTreeWidget::itemDoubleClicked,
         this, &QtFileExplorer::on_itemDoubleClicked);
 }
 
@@ -64,31 +69,59 @@ void QtFileExplorer::enter(const QString& _path)
     {
         auto& str = iter.next();
         QFileInfo fileInfo(iter.fileInfo());
-        auto item = new QTreeWidgetItem(ui.rightTreeWidget);
-        item->setText(Item::PATH, fileInfo.absoluteFilePath());
-        item->setText(Item::NAME, iter.fileName());
+        auto righTitem = new QTreeWidgetItem(ui.rightTreeWidget);
+
+        righTitem->setText(Item::PATH, fileInfo.absoluteFilePath());
+        righTitem->setText(Item::NAME, iter.fileName());
         if (fileInfo.isFile())
         {
-            item->setText(Item::SIZE, QString::number(fileInfo.size()));
+            righTitem->setText(Item::SIZE, QString::number(fileInfo.size()));
+        }
+        else
+        {
+            auto leftItem = new QTreeWidgetItem(ui.leftTreeWidget);
+            leftItem->setText(1, fileInfo.absoluteFilePath());
+            leftItem->setText(0, iter.fileName());
         }
     }
 }
 
 void QtFileExplorer::on_itemDoubleClicked(QTreeWidgetItem* item, int column)
 {
+    auto objectName = sender()->objectName();
     qDebug() << item->text(Item::NAME);
+    qDebug() << objectName;
 
-    QFileInfo fileInfo(item->text(Item::PATH));
-
-    if (item->text(Item::NAME) == "..")
+    if (objectName == "rightTreeWidget")
     {
-        enter(fileInfo.absolutePath());
-        return;
+        QFileInfo fileInfo(item->text(Item::PATH));
+
+        if (item->text(Item::NAME) == "..")
+        {
+            enter(fileInfo.absolutePath());
+            return;
+        }
+
+        if (fileInfo.isDir())
+        {
+            enter(fileInfo.absoluteFilePath());
+        }
     }
-
-    if (fileInfo.isDir())
+    else if (objectName == "leftTreeWidget")
     {
-        enter(fileInfo.absoluteFilePath());
+        QDirIterator iter(item->text(1), QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
+        while (iter.hasNext())
+        {
+            auto& str = iter.next();
+            QFileInfo fileInfo(iter.fileInfo());
+
+            if (fileInfo.isDir())
+            {
+                auto leftItem = new QTreeWidgetItem(item);
+                leftItem->setText(1, fileInfo.absoluteFilePath());
+                leftItem->setText(0, iter.fileName());
+            }
+        }
     }
 }
 
